8aec8d2d2122d0651f2eb6c4e08fbc01b50359a6
nikolascatur <nikolascatur@gmail.com>
2017-04-11 20:25:52 +0700

    -remove delete: file

--- app/src/androidTest/java/phone/nikolas/com/castflow/activity/main/MainMenuHandler.java
+++
@@ -1,8 +0,0 @@
-package phone.nikolas.com.castflow.activity.main;
-
-/**
- * Created by Pleret on 4/11/2017.
- */
-
-public class MainMenuHandler {
-}
--- app/src/androidTest/java/phone/nikolas/com/castflow/depen/component/AppComponent.java
+++
@@ -1,8 +0,0 @@
-package phone.nikolas.com.castflow.depen.component;
-
-/**
- * Created by Pleret on 4/11/2017.
- */
-
-public interface AppComponent {
-}
